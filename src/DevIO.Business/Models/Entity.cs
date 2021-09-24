58978e69155a63e358760da42b5e5f4397e16351
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2021-09-24 19:55:53 -0300

    Setup da aplicação

---
+++ src/DevIO.Business/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DevIO.Business.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
