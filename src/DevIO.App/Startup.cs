7192ae835f4c3e76f475f1c4cc58e803a5c3326a
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2021-09-27 20:59:43 -0300

    Implementação do Context na camada de acessos a dados

--- src/DevIO.App/Startup.cs
+++ src/DevIO.App/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using DevIO.App.Data;
+using DevIO.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -36,8 +37,11 @@ namespace DevIO.App
             });
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDbContext<MeuDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<IdentityUser>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,29 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(string))))
+                property.Relational().ColumnType = "varchar(100)";
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
